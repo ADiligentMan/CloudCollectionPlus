b9a763df9a68bc624c86dd7cacd27c7d8523178e
ADiligentMan <1003638818@qq.com>
2018-06-19 17:04:45 +0800

    修改了点小东西

--- src/com/collectionplus/utils/Aspect.java
+++ src/com/collectionplus/utils/Aspect.java
@@ -8,6 +8,11 @@ import javax.servlet.http.HttpServletRequest;
 
 import com.collectionplus.bean.ReturnModel;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureException;
+
 public class Aspect {
 
 	public static void before(Object req) {
@@ -30,7 +35,7 @@ public class Aspect {
 		//	去掉最后一个&
 		String s = sb.substring(0,sb.length()-1);
 		
-		System.out.println(time + " " + "Request:" + s);
+		System.out.println(time + " " + "请求URL:" + s);
 	}
 	
 	public static void afterReturning(Object result) {
@@ -42,5 +47,28 @@ public class Aspect {
 		
 		System.out.println(output);
 	}
+	
+	public boolean isJwtValid(String jwt) {
+		  try {
+		  // 解析JWT字符串中的数据，并进行最基础的验证
+		  Claims claims = Jwts.parser()
+		  .setSigningKey("")  //SECRET_KEY是加密算法对应的密钥，jjwt可以自动判断机密算法；
+		  .parseClaimsJws(jwt)   //jwt是客户端生成的JWT字符串；
+		  .getBody();
+		  String vaule = claims.get("key", String.class);   //获取自定义字段key；
+
+		  // 判断自定义字段是否正确
+		  if ("vaule".equals(vaule)) {
+		  return true;
+		  } else {
+		  return false;
+		  }
+		  }
+		  //在解析JWT字符串时，如果密钥不正确，将会解析失败，抛出SignatureException异常，说明该JWT字符串是伪造的；
+		  //在解析JWT字符串时，如果‘过期时间字段’已经早于当前时间，将会抛出ExpiredJwtException异常，说明本次请求已经失效；
+		  catch (SignatureException|ExpiredJwtException e) {
+		  return false;
+		  }
+		}
 
 }
